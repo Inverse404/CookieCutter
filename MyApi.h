8e51a0a1fc775a85b56fe2b4cda58de55e62e09c
Niels Seidler <seidler@computer-science.de>
2020-02-16 19:28:09 +0100

    fixed the platform specific file url handling macro to actually work on both platforms (windows/linux)

--- MyApi.h
+++ MyApi.h
@@ -4,9 +4,9 @@
 #include <QUrl>
 
 #ifdef Q_OS_WIN
-#define REMOVE_FILE_PROTOCOL_PREFIX "\"file:///\""
+#define REMOVE_FILE_PROTOCOL_PREFIX "file:///"
 #else
-#define REMOVE_FILE_PROTOCOL_PREFIX "\"file://\""
+#define REMOVE_FILE_PROTOCOL_PREFIX "file://"
 #endif
 
 class MyApi : public QObject
@@ -17,9 +17,9 @@ public:
 	MyApi(MyApi const& other) : QObject() {};
 	Q_INVOKABLE void saveCookie(QString const& sourcePath, QString const& targetPath, int x, int y, int w, int h, bool rescale = false, int outW = 0, int outH = 0) {
 		QString localInPath = sourcePath;
-        localInPath.remove("file://");
+        localInPath.remove(REMOVE_FILE_PROTOCOL_PREFIX);
 		QString localOutPath = targetPath;
-        localOutPath.remove("file://");
+        localOutPath.remove(REMOVE_FILE_PROTOCOL_PREFIX);
 		QImage source(localInPath);
 		if (!source.isNull()) {
 			QImage cookie = source.copy(x, y ,w, h);

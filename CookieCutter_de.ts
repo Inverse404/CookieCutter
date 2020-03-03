4e18e68d096b4dde7d4b718d467306d906313058
Niels Sönke Seidler <developer@behringer-touristik.de>
2020-03-03 16:11:57 +0100

    added 'de' translation for tutorial popups

--- CookieCutter_de.ts
+++ CookieCutter_de.ts
@@ -4,157 +4,157 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="main.qml" line="32"/>
+        <location filename="main.qml" line="31"/>
         <source>CookieCutter</source>
         <translatorcomment>Eigenname des Programms wird nicht lokalisiert</translatorcomment>
         <translation>CookieCutter</translation>
     </message>
     <message>
-        <location filename="main.qml" line="237"/>
+        <location filename="main.qml" line="258"/>
         <source>info</source>
         <translation>Info</translation>
     </message>
     <message>
-        <location filename="main.qml" line="252"/>
+        <location filename="main.qml" line="275"/>
         <source>pick input image...</source>
         <translation>Quellbild wählen...</translation>
     </message>
     <message>
-        <location filename="main.qml" line="261"/>
+        <location filename="main.qml" line="284"/>
         <source>cookie shapes (%1)</source>
         <translation>Ausstechförmchen (%1)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="295"/>
+        <location filename="main.qml" line="318"/>
         <source> (%L1:%L2)</source>
         <translation> (%L1:%L2)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="333"/>
+        <location filename="main.qml" line="356"/>
         <source>golden cut</source>
         <translation>Goldener Schnitt</translation>
     </message>
     <message>
-        <location filename="main.qml" line="336"/>
-        <location filename="main.qml" line="343"/>
-        <location filename="main.qml" line="350"/>
-        <location filename="main.qml" line="357"/>
-        <location filename="main.qml" line="364"/>
-        <location filename="main.qml" line="371"/>
-        <location filename="main.qml" line="378"/>
-        <location filename="main.qml" line="385"/>
-        <location filename="main.qml" line="392"/>
-        <location filename="main.qml" line="399"/>
-        <location filename="main.qml" line="406"/>
-        <location filename="main.qml" line="413"/>
-        <location filename="main.qml" line="420"/>
-        <location filename="main.qml" line="427"/>
-        <location filename="main.qml" line="434"/>
+        <location filename="main.qml" line="359"/>
+        <location filename="main.qml" line="366"/>
+        <location filename="main.qml" line="373"/>
+        <location filename="main.qml" line="380"/>
+        <location filename="main.qml" line="387"/>
+        <location filename="main.qml" line="394"/>
+        <location filename="main.qml" line="401"/>
+        <location filename="main.qml" line="408"/>
+        <location filename="main.qml" line="415"/>
+        <location filename="main.qml" line="422"/>
+        <location filename="main.qml" line="429"/>
+        <location filename="main.qml" line="436"/>
+        <location filename="main.qml" line="443"/>
+        <location filename="main.qml" line="450"/>
+        <location filename="main.qml" line="457"/>
         <source>standard cookies</source>
         <translation>Standardförmchen</translation>
     </message>
     <message>
-        <location filename="main.qml" line="340"/>
+        <location filename="main.qml" line="363"/>
         <source>square</source>
         <translation>Quadrat</translation>
     </message>
     <message>
-        <location filename="main.qml" line="347"/>
+        <location filename="main.qml" line="370"/>
         <source>univisium</source>
         <translation>Univisium</translation>
     </message>
     <message>
-        <location filename="main.qml" line="354"/>
+        <location filename="main.qml" line="377"/>
         <source>35mm film</source>
         <translation>35mm Film</translation>
     </message>
     <message>
-        <location filename="main.qml" line="361"/>
+        <location filename="main.qml" line="384"/>
         <source>APS Panorama</source>
         <translation>APS-Panorama</translation>
     </message>
     <message>
-        <location filename="main.qml" line="368"/>
+        <location filename="main.qml" line="391"/>
         <source>legacy photo</source>
         <translation>Foto (analog)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="375"/>
+        <location filename="main.qml" line="398"/>
         <source>legacy screen</source>
         <translation>Bildschirm (analog)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="382"/>
-        <location filename="main.qml" line="389"/>
+        <location filename="main.qml" line="405"/>
+        <location filename="main.qml" line="412"/>
         <source>digital screen</source>
         <translation>Bildschirm (digital)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="396"/>
+        <location filename="main.qml" line="419"/>
         <source>mobile screen portrait</source>
         <translation>Selfie, hochkant</translation>
     </message>
     <message>
-        <location filename="main.qml" line="403"/>
+        <location filename="main.qml" line="426"/>
         <source>widescreen film</source>
         <translation>Breitbild (Film)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="410"/>
-        <location filename="main.qml" line="417"/>
+        <location filename="main.qml" line="433"/>
+        <location filename="main.qml" line="440"/>
         <source>anamorphic film</source>
         <translation>Anamorph (Film)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="424"/>
+        <location filename="main.qml" line="447"/>
         <source>ultra panavision film</source>
         <translation>Ultra Panavision (Film)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="431"/>
+        <location filename="main.qml" line="454"/>
         <source>polyvision film</source>
         <translation>Polyvision (Film)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="468"/>
+        <location filename="main.qml" line="491"/>
         <source>custom aspect ratio</source>
         <translation>eigene Form</translation>
     </message>
     <message>
-        <location filename="main.qml" line="486"/>
-        <location filename="main.qml" line="551"/>
+        <location filename="main.qml" line="509"/>
+        <location filename="main.qml" line="574"/>
         <source>width</source>
         <translation>Breite</translation>
     </message>
     <message>
-        <location filename="main.qml" line="505"/>
-        <location filename="main.qml" line="569"/>
+        <location filename="main.qml" line="528"/>
+        <location filename="main.qml" line="592"/>
         <source>height</source>
         <translation>Höhe</translation>
     </message>
     <message>
-        <location filename="main.qml" line="532"/>
+        <location filename="main.qml" line="555"/>
         <source>custom output size</source>
         <translation>eigene Ausgabegröße</translation>
     </message>
     <message>
-        <location filename="main.qml" line="579"/>
+        <location filename="main.qml" line="602"/>
         <source>pixels</source>
         <translation>Pixel</translation>
     </message>
     <message>
-        <location filename="main.qml" line="593"/>
-        <location filename="main.qml" line="610"/>
+        <location filename="main.qml" line="616"/>
+        <location filename="main.qml" line="633"/>
         <source>Images (*.jpg *.jpeg *.png *.bmp)</source>
         <translation>Bilder (*.jpg *.jpeg *.png *.bmp)</translation>
     </message>
     <message>
-        <location filename="main.qml" line="631"/>
+        <location filename="main.qml" line="654"/>
         <source>CookieCutter - quick image cut outs</source>
         <translation>CookieCutter - einfache Bildausschnitte</translation>
     </message>
     <message>
-        <location filename="main.qml" line="637"/>
+        <location filename="main.qml" line="660"/>
         <source>(c) 2020 Niels Sönke Seidler
 
 This software is licensed under the GPL v3
@@ -163,6 +163,36 @@ This software is licensed under the GPL v3
 
 Diese Software ist lizenziert unter der GPL v3
 (siehe beiliegende Datei &apos;gpl-3.0.txt&apos; oder &apos;http://https://www.gnu.org/licenses/gpl-3.0&apos;)</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="714"/>
+        <source>Step 1: Pick a source image to cut pieces from.</source>
+        <translation>Schritt 1: Quelldatei auswählen.</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="739"/>
+        <source>Step 2: Choose a cookie cutout shape.</source>
+        <translation>Schritt 2: Form auswählen.</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="774"/>
+        <source>Step 3: Move mouse over the source image to aim.</source>
+        <translation>Schritt 3: Mit der Maus auf einen Bildbereich zielen.</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="805"/>
+        <source>Step 4: Use mouse wheel to change cutout size.</source>
+        <translation>Schritt4: Mit Mausrad die Größe anpassen.</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="812"/>
+        <source>Hold shift while turning the mouse wheel for higher precision.</source>
+        <translation>Shift-Taste halten, um die Größe zielgenau zu verändern.</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="843"/>
+        <source>Step 5: Click at the desired position to save your cutout to a new file.</source>
+        <translation>Schritt 5: An die gewünschte Stelle klicken, um den Auschnitt in eine neue Datei zu speichern.</translation>
     </message>
 </context>
 </TS>
